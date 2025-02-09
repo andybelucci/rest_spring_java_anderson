dd340059eb70e1d1da3c855839f96770943beff4
andybelucci <beluccianderson@gmail.com>
2025-02-09 11:59:04 -0300

    Projeto iniciado

---
+++ src/main/java/br/com/anderson/rest_spring_java_anderson/Greeting.java
@@ -0,0 +1,21 @@
+package br.com.anderson.rest_spring_java_anderson;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
