d54ea531e05e70939e8f1a835b0a2f5c8dbc44e9
andybelucci <beluccianderson@gmail.com>
2025-02-11 23:54:33 -0300

    Tratamento de exceções

---
+++ src/main/java/br/com/anderson/rest_spring_java_anderson/exceptions/UnsupportedMathOperationException.java
@@ -0,0 +1,13 @@
+package br.com.anderson.rest_spring_java_anderson.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class UnsupportedMathOperationException extends RuntimeException{
+
+    public UnsupportedMathOperationException(String ex) {
+        super(ex);
+    }
+
+}
